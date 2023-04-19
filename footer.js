ae8216d85bfe901b9fd94c7acf218eb62f958a02
jhoitenga <116404784+jhoitenga@users.noreply.github.com>
2023-04-19 14:29:08 -0500

    updating youtube link

--- footer.js
+++ footer.js
@@ -10,7 +10,7 @@ class Footer extends HTMLElement
                 <p>
                 <a href="https://github.com/jhoitenga" target="_blank">My GitHub</a>
                 <a href="https://www.bellevue.edu/degrees/bachelor/web-development-bs/" target="_blank">Bellevue Web Development Degree</a>
-                <a href="">WEB-330 YouTube</a>
+                <a href="https://www.youtube.com/user/bellevueuniversity" target="_blank">Bellevue University YouTube</a>
                 <a href="https://github.com/buwebdev/web-330" target="_blank">WEB-330 GitHub</a> 
                 </p>
             </div>
