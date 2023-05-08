b7b861018e3e115cedfa67a6e0be5d67762f3ea5
jhoitenga <116404784+jhoitenga@users.noreply.github.com>
2023-05-08 10:38:28 -0500

    code cleanup

--- footer.js
+++ footer.js
@@ -7,12 +7,10 @@ class Footer extends HTMLElement
     connectedCallback() {
         this.innerHTML = `
             <div id="footer">
-                <p>
                 <a href="https://github.com/jhoitenga" target="_blank">My GitHub</a>
                 <a href="https://www.bellevue.edu/degrees/bachelor/web-development-bs/" target="_blank">Bellevue Web Development Degree</a>
                 <a href="https://www.youtube.com/user/bellevueuniversity" target="_blank">Bellevue University YouTube</a>
                 <a href="https://github.com/buwebdev/web-330" target="_blank">WEB-330 GitHub</a> 
-                </p>
             </div>
             `;
     }
